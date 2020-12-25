9dc8019414684b7d0b4884a75016e9f48379694e
Patrick Pace <patrickqpace@gmail.com>
2020-12-25 15:19:08 -0500

    Basic sound file listing

--- benny.py
+++ benny.py
@@ -4,6 +4,7 @@ import pymumble_py3 as pymumble
 from pymumble_py3.callbacks import PYMUMBLE_CLBK_TEXTMESSAGERECEIVED as PCM
 import json
 import subprocess as sp
+import os
 from os import path
 
 # Update: exception, error handling
@@ -29,12 +30,15 @@ def validate(filename):
             if path.exists(filename + t):
                 return filename + t
 
+def actor_message(actor, message):
+    actor.send_text_message(message)
+
 # Send a message into the active channel.
 def channel_message(message):
-    pass
+    mumble.my_channel().send_text_message(message)
 
 # Play a sound file.
-def cmd_play(a):
+def cmd_play(msg, a):
     # Check that array is only one element
     # Check that element is a valid filename
     # Play it.
@@ -59,6 +63,8 @@ def cmd_play(a):
     command = ["ffmpeg", "-i", filename, "-filter:a", "volume=" + str(volume), "-acodec", "pcm_s16le", "-f", "s16le", "-ab", "192k", "-ac", "1", "-ar", "48000", "-"]
     sound = sp.Popen(command, stdout=sp.PIPE, stderr=sp.DEVNULL, bufsize=1024)
 
+    channel_message("Playing.")
+
     playing = True
     # Change into callback?
     while playing:
@@ -67,15 +73,46 @@ def cmd_play(a):
             break
         mumble.sound_output.add_sound(raw_music)
 
-def cmd_stop(a):
+def cmd_stop(msg, a):
     global playing
 
-    # TODO: Print message
+    channel_message("Stopping.")
+
     playing = False
     mumble.sound_output.clear_buffer()
 
-def cmd_list(a):
-    pass
+
+# List pushing with limit testing
+def list_join(l, elem):
+    # FIXME: list max magic number
+    if len(l) >= 50:
+        return
+
+    l.append(elem)
+
+# Print a list of the files in the sound library.
+def cmd_list(msg, a):
+    actor = mumble.users.get(msg.actor)
+
+    library_path = config.get("library").get("path")
+
+    l = []
+
+    for root, dirs, files, in os.walk(library_path, topdown=True):
+        for name in files:
+            list_join(l, os.path.join(root, name))
+
+        # NOTE: Not sure that we need to list directory names
+        #for name in dirs:
+        #    list_join(l, os.path.join(root, name))
+
+    s = "\n"
+    for name in l:
+        s += "<p>" + name + "</p>"
+
+    actor.send_text_message(s)
+    print(l)
+
 
 aliases = {
     'bp': cmd_play,
@@ -83,7 +120,7 @@ aliases = {
     'bl': cmd_list,
 }
 
-def process_command(a):
+def process_command(msg, a):
     # There are two ways to input commands:
     # benny + <cmd> + <arg1> + <arg2>, benny stop...
     # or bp <arg1>, bs...
@@ -95,7 +132,7 @@ def process_command(a):
     for alias in aliases:
        if cmd == alias:
            found = True
-           aliases[cmd](a)
+           aliases[cmd](msg, a)
 
     if not found:
         print("Tried to find command " + cmd + " but couldn't find it")
@@ -109,7 +146,7 @@ def process_message(msg):
     message = msg.message
 
     a = message.split()
-    process_command(a)
+    process_command(msg, a)
 
 mumble = pymumble.Mumble(config.get("server"), config.get("nick"), password=config.get("password"), port=int(config.get("port")))
 mumble.callbacks.set_callback(PCM, process_message)
